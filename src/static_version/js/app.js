9a1b56fe14aca4e9c2958cc18e042f6341a2892e
Noah Weinthal <noah@freehand.local>
2016-04-16 19:25:55 -0400

    Prolly gonna wanna roll this one back at some point

--- src/static_version/js/app.js
+++ src/static_version/js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+$(document).ready(function(){
+    $.ajax({
+        method: "GET",
+        url: "http://parentsfiles.s3.amazonaws.com"
+    }).done(function(response){
+        console.log(response);
+    }); 
+});
